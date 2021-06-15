21900d7ab3393fe97337e0373ba5b7651f63a96d
oswaldo-valadez-qmc <79599052+oswaldo-valadez-qmc@users.noreply.github.com>
2021-06-15 09:39:01 -0500

    ok

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,9 +2,26 @@
 
 const ErrorHandler = require("../utils/helpers/error-handler");
 
+const Brands = require("../models/brands.model");
+const Categories = require("../models/categories.model");
+const Subcategories = require("../models/subcategories.model");
+const Measurementunits = require("../models/measurementunits.model");
+const Materials = require("../models/materials.model");
+
 exports.renderDashboard = async (req, res, next) => {
   try {
-    res.render("dashboard");
+    const brands = await Brands.getBrandsCount();
+    const categories = await Categories.getCategoriesCount();
+    const subcategories = await Subcategories.getSubcategoriesCount();
+    const measurementunits = await Measurementunits.getMeasurementunitsCount();
+
+    const total_materials = await Materials.getMaterialsCount();
+
+    const total_catalogs =
+      brands + categories + subcategories + measurementunits;
+
+    console.log(total_catalogs);
+    res.render("dashboard", { total_catalogs, total_materials });
   } catch (error) {
     ErrorHandler.handleError(req, res, error);
   }
