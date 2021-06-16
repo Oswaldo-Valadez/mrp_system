e8b8650c720de2c3ea2e5853bb2f639cb85900d5
oswaldo-valadez-qmc <79599052+oswaldo-valadez-qmc@users.noreply.github.com>
2021-06-16 12:21:15 -0500

    inserts ok

---
+++ src/utils/database/sql/inserts.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`id_user`, `username`, `fullname`, `password`) VALUES
+(1, 'admin', 'Oswaldo Dasaet Valadez Láriz', '$2a$10$A2vidM7fdPz9pm92gWJZzOwX2yIaJofkxWaqYGavsVAvxA4LpEoZ6');
