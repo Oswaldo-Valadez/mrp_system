6d1bae54c59d91af2dafbf0d0d1bfcdfdb0ec58c
oswaldo-valadez-qmc <79599052+oswaldo-valadez-qmc@users.noreply.github.com>
2021-06-22 12:16:14 -0500

    small changes ok

--- src/utils/database/pool.js
+++ src/utils/database/pool.js
@@ -11,9 +11,9 @@ const pool = mysql.createPool(database);
 pool.getConnection((err, connection) => {
   if (err) {
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.');
+      console.error('Database connection was closed');
     } else if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has to many connections');
+      console.error('Database has too many connections');
     } else if (err.code === 'ECONNREFUSED') {
       console.error('Database connection was refused');
     } else {
