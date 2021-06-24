f77d8eebf5e14ae8aca87e3c7248055a0536b08c
MiguelGuillen2000 <up180120@alumnos.upa.edu.mx>
2021-06-23 22:14:04 -0500

    base de datos

--- src/utils/database/sql/mrp_system.sql
+++ src/utils/database/sql/mrp_system.sql
@@ -26,59 +26,67 @@ USE `mrp_system`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `brands`
+-- Table structure for table `marcas`
 --
 
-DROP TABLE IF EXISTS `brands`;
-CREATE TABLE `brands` (
-  `id_brand` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `pin_up` tinyint(1) NOT NULL DEFAULT 0
+DROP TABLE IF EXISTS `marcas`;
+CREATE TABLE `marcas` (
+  `id_marca` int(11) NOT NULL,
+  `descripcion` varchar(30) NOT NULL
+  -- `name` varchar(100) NOT NULL,
+  -- `pin_up` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table `categorias`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE `categories` (
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE `categorias` (
   `id_category` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `pin_up` tinyint(1) NOT NULL DEFAULT 0
+  -- `name` varchar(100) NOT NULL,
+  `description` text NOT NULL
+-- `pin_up` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `components`
+-- Table structure for table `componentes`
 --
 
-DROP TABLE IF EXISTS `components`;
-CREATE TABLE `components` (
-  `id_component` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `stock` int(11) NOT NULL,
-  `id_brand` int(11) NOT NULL,
-  `id_measurement_unit` int(11) NOT NULL,
-  `id_category` int(11) NOT NULL
+DROP TABLE IF EXISTS `componentes`;
+CREATE TABLE `componentes` (
+  `id_componente` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_unidad` int(11) NOT NULL,
+  `id_marca` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `dias de entrega` SMALLINT,
+  `existencias` int NOT NULL,
+  `stock_minimo` int NOT NULL
+  -- `name` varchar(100) NOT NULL,
+  -- `description` text NOT NULL,
+  -- `stock` int(11) NOT NULL,
+  -- `id_brand` int(11) NOT NULL,
+  -- `id_measurement_unit` int(11) NOT NULL,
+  -- `id_category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `measurement_units`
+-- Table structure for table `unidades de medida`
 --
 
-DROP TABLE IF EXISTS `measurement_units`;
-CREATE TABLE `measurement_units` (
-  `id_measurement_unit` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(5) NOT NULL,
-  `pin_up` tinyint(1) NOT NULL DEFAULT 0
+DROP TABLE IF EXISTS `unidades`;
+CREATE TABLE `unidades` (
+  `id_unidad` int(11) NOT NULL,
+  `unidad` varchar(50) NOT NULL
+  -- `code` varchar(5) NOT NULL,
+  -- `pin_up` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -88,9 +96,9 @@ CREATE TABLE `measurement_units` (
 --
 
 DROP TABLE IF EXISTS `mps`;
-CREATE TABLE `mps` (
-  `id_component` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--CREATE TABLE `mps` (
+--  `id_component` int(11) NOT NULL
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -98,181 +106,230 @@ CREATE TABLE `mps` (
 -- Table structure for table `mps_periods`
 --
 
-DROP TABLE IF EXISTS `mps_periods`;
-CREATE TABLE `mps_periods` (
-  `id_mps_period` int(11) NOT NULL,
-  `gross_requirement` int(11) NOT NULL,
-  `scheduled_receptions` int(11) NOT NULL,
-  `availability_projection` int(11) NOT NULL,
-  `net_requirements` int(11) NOT NULL,
-  `planned_order_release` int(11) NOT NULL
+-- DROP TABLE IF EXISTS `mps_periods`;
+-- CREATE TABLE `mps_periods` (
+--   `id_mps_period` int(11) NOT NULL,
+--   `gross_requirement` int(11) NOT NULL,
+--   `scheduled_receptions` int(11) NOT NULL,
+--   `availability_projection` int(11) NOT NULL,
+--   `net_requirements` int(11) NOT NULL,
+--   `planned_order_release` int(11) NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordenes`
+--
+
+DROP TABLE IF EXISTS `ordenes`;
+CREATE TABLE `ordenes` (
+  `id_orden` int(11) NOT NULL,
+  `cliente` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordenes_partida`
+--
+
+DROP TABLE IF EXISTS `ordenes_partida`;
+CREATE TABLE `ordenes_partida` (
+  `id_orden` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha de entrega` date,
+  `estatus` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `id_hoja` int(11) NOT NULL,
+  `existencias` int(11) NOT NULL,
+  `dias_en_elaboracion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `hoja_calculo`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-  `id_order` int(11) NOT NULL,
-  `order_reference` varchar(20) NOT NULL
+DROP TABLE IF EXISTS `hoja_calculo`;
+CREATE TABLE `hoja_calculo` (
+  `id_hoja` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_componente` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE `products` (
-  `id_product` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `spreadsheet`
+-- Table structure for table `plan maestro`
 --
 
-DROP TABLE IF EXISTS `spreadsheet`;
-CREATE TABLE `spreadsheet` (
-  `id_component` int(11) NOT NULL,
-  `id_product` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
+DROP TABLE IF EXISTS `plan_maestro`;
+CREATE TABLE `plan_maestro` (
+  `id_mes` int(11) NOT NULL,
+  `id_componente` int(11) NOT NULL,
+  `inventario_ini` int(11) NOT NULL,
+  `unidad_prono` int(11) NOT NULL,
+  `ped_client` int(11) NOT NULL,
+  `inventario_final` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- CREATE TABLE `users` (
+--   `id_user` int(11) NOT NULL,
+--   `username` varchar(20) NOT NULL,
+--   `fullname` varchar(100) NOT NULL,
+--   `password` varchar(255) NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `brands`
+-- Indexes for table `marca`
 --
-ALTER TABLE `brands`
-  ADD PRIMARY KEY (`id_brand`);
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id_marca`);
 
 --
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id_category`);
+  ADD PRIMARY KEY (`id_categoria`);
 
 --
 -- Indexes for table `components`
 --
 ALTER TABLE `components`
-  ADD PRIMARY KEY (`id_component`),
-  ADD KEY `id_brand` (`id_brand`),
-  ADD KEY `id_measurement_unit` (`id_measurement_unit`),
-  ADD KEY `id_category` (`id_category`);
+  ADD PRIMARY KEY (`id_componente`),
+  ADD KEY `id_marca` (`id_marca`),
+  ADD KEY `id_unidad` (`id_unidad`),
+  ADD KEY `id_categoria` (`id_categoria`);
 
 --
 -- Indexes for table `measurement_units`
 --
-ALTER TABLE `measurement_units`
-  ADD PRIMARY KEY (`id_measurement_unit`);
+ALTER TABLE `unidades`
+  ADD PRIMARY KEY (`id_unidad`);
 
 --
 -- Indexes for table `mps_periods`
 --
-ALTER TABLE `mps_periods`
-  ADD PRIMARY KEY (`id_mps_period`);
+--ALTER TABLE `mps_periods`
+--  ADD PRIMARY KEY (`id_mps_period`);
 
 --
 -- Indexes for table `orders`
 --
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id_order`);
+ALTER TABLE `ordenes`
+  ADD PRIMARY KEY (`id_orden`);
 
 --
 -- Indexes for table `products`
 --
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id_product`);
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
 
 --
 -- Indexes for table `spreadsheet`
 --
-ALTER TABLE `spreadsheet`
-  ADD KEY `id_component` (`id_component`),
-  ADD KEY `id_product` (`id_product`);
+ALTER TABLE `hoja_calculo`
+  ADD PRIMARY KEY (`id_hoja`),
+  ADD KEY `id_componente` (`id_componente`),
+  ADD KEY `id_producto` (`id_producto`);
 
+-- Indexes for table `ordenes_partida`
+--
+ALTER TABLE `ordenes_partida`
+  ADD PRIMARY KEY (`id_orden, id_producto`);
+
+-- Indexes for table `plan maestro`
+--
+ALTER TABLE `plan_maestro`
+  ADD PRIMARY KEY (`id_mes, id_componente`);
 --
 -- Indexes for table `users`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
+--ALTER TABLE `users`
+--  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+--
+-- AUTO_INCREMENT for table `hoja calculo`
+--
+ALTER TABLE `hoja_calculo`
+  MODIFY `id_hoja` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `brands`
 --
-ALTER TABLE `brands`
-  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `marcas`
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
-ALTER TABLE `categories`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `components`
 --
-ALTER TABLE `components`
-  MODIFY `id_component` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `componente`
+  MODIFY `id_componente` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `measurement_units`
 --
-ALTER TABLE `measurement_units`
-  MODIFY `id_measurement_unit` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `unidades`
+  MODIFY `id_unidad` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `mps_periods`
 --
-ALTER TABLE `mps_periods`
-  MODIFY `id_mps_period` int(11) NOT NULL AUTO_INCREMENT;
+--ALTER TABLE `mps_periods`
+--  MODIFY `id_mps_period` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `ordenes`
+  MODIFY `id_orden` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
-ALTER TABLE `products`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+--ALTER TABLE `users`
+--  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -281,17 +338,36 @@ ALTER TABLE `users`
 --
 -- Constraints for table `components`
 --
-ALTER TABLE `components`
-  ADD CONSTRAINT `components_ibfk_1` FOREIGN KEY (`id_brand`) REFERENCES `brands` (`id_brand`),
-  ADD CONSTRAINT `components_ibfk_2` FOREIGN KEY (`id_measurement_unit`) REFERENCES `measurement_units` (`id_measurement_unit`),
-  ADD CONSTRAINT `components_ibfk_3` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`);
+ALTER TABLE `componentes`
+  ADD CONSTRAINT `components_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id_marca`),
+  ADD CONSTRAINT `components_ibfk_2` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`id_unidad`),
+  ADD CONSTRAINT `components_ibfk_3` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`);
 
 --
 -- Constraints for table `spreadsheet`
 --
-ALTER TABLE `spreadsheet`
-  ADD CONSTRAINT `spreadsheet_ibfk_1` FOREIGN KEY (`id_component`) REFERENCES `components` (`id_component`),
+ALTER TABLE `hoja_calculo`
+  ADD CONSTRAINT `spreadsheet_ibfk_1` FOREIGN KEY (`id_componente`) REFERENCES `componentes` (`id_componente`),
   ADD CONSTRAINT `spreadsheet_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`);
+COMMIT;
+
+-- Constraints for table `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_hoja`) REFERENCES `hoja_calculo` (`id_hoja`);
+COMMIT;
+
+-- Constraints for table `ordenes partida`
+--
+ALTER TABLE `ordenes_partida`
+  ADD CONSTRAINT `ordenes_partida_ibfk_1` FOREIGN KEY (`id_orden`) REFERENCES `ordenes` (`id_orden`),
+  ADD CONSTRAINT `ordenes_partida_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+COMMIT;
+
+-- Constraints for table `plan maestro`
+--
+ALTER TABLE `plan_maestro`
+  ADD CONSTRAINT `plan_maestro_ibfk_1` FOREIGN KEY (`id_componente`) REFERENCES `componentes` (`id_componente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
